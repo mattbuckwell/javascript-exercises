a68be3bdbdc6feea38fd3185f65b45d7bacff571
Matt Buckwell <mattbuckwell@gmail.com>
2024-11-09 15:59:19 +1300

    Completed fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
 
+    if (start > end) {
+        start += end;
+        end = start - end;
+        start -= end;
+    }
+
+    let total = 0;
+    for(let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
