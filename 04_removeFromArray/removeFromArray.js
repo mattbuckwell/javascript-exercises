ca3510486d8b253864f723f04791016d67fff2fd
Matt Buckwell <mattbuckwell@gmail.com>
2024-11-09 15:04:32 +1300

    Completed forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeNum) {
+    let newArray = [];
+    let result = false;
+    for(let i = 0; i < array.length; i++) {
+        for(const remove of removeNum) {
+            if(array[i] === remove) {
+                result = true;
+            }
+        }
+        if (result === true) {
+            result = false;
+            continue;
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
