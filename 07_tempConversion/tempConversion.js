e24867470cbc7e5a9d3fa03b56f94c7b54a04045
Matt Buckwell <mattbuckwell@gmail.com>
2024-11-09 16:47:16 +1300

    Completed seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius*9/5 + 32);
+  if (result % 2 !== 0 ) {
+    return Number.parseFloat(result.toFixed(1));
+  }
+  return result;
 };
 
 // Do not edit below this line
